773f034a28a14772b7d8c2707ade3afc3b2a68ec
Anisha <bhattacharyaanisha2001@gmail.com>
2022-03-20 16:44:53 -0400

    implement

--- mlp.py
+++ mlp.py
@@ -57,6 +57,66 @@ class MLP:
         return 2*(1/(1+np.exp(-a)))*(2*(a)) - 1
 
 
+    #backprop:
+    #derivative of tan h is 1-tan^2
+    def tan_derivative(output):
+        return 1- output**2
+
+
+# error = (weight[k] * error[j]) * tan_derivative(output)
+
+def backward_propagate_error(network, expected):
+	for i in reversed(range(len(network))):
+		layer = network[i]
+		errors = list()
+		if i != len(network)-1:
+			for j in range(len(layer)):
+				error = 0.0
+				for neuron in network[i + 1]:
+					error += (neuron['weights'][j] * neuron['delta'])
+				errors.append(error)
+		else:
+			for j in range(len(layer)):
+				neuron = layer[j]
+				errors.append(neuron['output'] - expected[j])
+		for j in range(len(layer)):
+			neuron = layer[j]
+			neuron['delta'] = errors[j] * tan_derivative(neuron['output'])
+
+# Update network weights with error
+def update_weights(network, row, l_rate):
+	for i in range(len(network)):
+		inputs = row[:-1]
+		if i != 0:
+			inputs = [neuron['output'] for neuron in network[i - 1]]
+		for neuron in network[i]:
+			for j in range(len(inputs)):
+				neuron['weights'][j] -= l_rate * neuron['delta'] * inputs[j]
+			neuron['weights'][-1] -= l_rate * neuron['delta']
+
+def train_network(network, train, l_rate, n_epoch, n_outputs):
+	for epoch in range(n_epoch):
+		sum_error = 0
+		for row in train:
+			outputs = forward_propagate(network, row)
+			expected = [0 for i in range(n_outputs)]
+			expected[row[-1]] = 1
+			sum_error += sum([(expected[i]-outputs[i])**2 for i in range(len(expected))])
+			backward_propagate_error(network, expected)
+			update_weights(network, row, l_rate)
+		print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+    
+
+
+
+    
+
+    
+
+
+        
+
+
 
 
  
